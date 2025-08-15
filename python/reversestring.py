26ca3bdd62d017b222a030d8802d9358fcb30a92
prasanna jajam <prasannajajam54@gmail.com>
2025-08-15 21:02:21 +0530

    Add files via upload

---
+++ python/reversestring.py
@@ -0,0 +1,9 @@
+#reverse string#
+string=input("enter a string:")
+reverse=""
+for i in range(len(string)):
+      reverse=string[i]+reverse
+print(reverse)
+
+#input:enter a string:prasannajajam#
+#output:majajannasarp#
