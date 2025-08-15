43e05deaec718462dca5012fdb94c14086f7cec6
prasanna jajam <prasannajajam54@gmail.com>
2025-08-15 21:01:57 +0530

    Add files via upload

---
+++ python/removeall spaces from string.py
@@ -0,0 +1,7 @@
+#remove all spaces from a string#
+original_string ="This is a string with spaces."
+string_without_spaces = original_string.replace(" ", "")
+print(string_without_spaces)
+
+
+#output:Thisisastringwithspaces.#
