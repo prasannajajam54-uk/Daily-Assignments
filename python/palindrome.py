222e2d2893c9a9000a4e1b87b26ce9a6684bcc55
prasanna jajam <prasannajajam54@gmail.com>
2025-08-15 21:01:27 +0530

    Add files via upload

---
+++ python/palindrome.py
@@ -0,0 +1,13 @@
+#check the given string is palindrme or not#
+str=input("enter a string:")
+reverse=str[::-1]
+if reverse==str:
+    print("palindrome")
+else:
+    print("not palindrome")
+
+#input:madam#
+#output:enter a string:madam
+#palindrome#
+
+
